bfe49cc3c76d064df9c58c85ce5c370d7d586e9e
GustavoSilverio <gustavosk290905@gmail.com>
2024-06-15 12:18:20 -0300

    config + init + base structure

---
+++ src/components/GradientElipse/GradientElipse.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils"
+import { GradientElipseProps } from "@/types/components/GradientElipse"
+
+export const GradientElipse = ({ side }: GradientElipseProps) => {
+	return (
+		<div
+			className={cn(
+				"w-[400px] h-[400px] bg-slate-700 blur-[230px] rounded-full fixed",
+				{
+					"top-[-250px] left-[-250px]": side === "top",
+					"bottom-[-250px] right-[-250px]": side === "bottom",
+				}
+			)}
+		/>
+	)
+}
---
+++ src/components/GradientElipse/index.ts
@@ -0,0 +1,1 @@
+export { GradientElipse } from "./GradientElipse"
