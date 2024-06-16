a0d5408aa157835bd313d4f93c9d293160a8e93a
GustavoSilverio <gustavosk290905@gmail.com>
2024-06-15 23:26:27 -0300

    responsivity footer correction

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,23 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { usePathname } from "next/navigation"
+
 export const Footer = () => {
 	const actualYear = new Date().getFullYear()
+	const pathName = usePathname()
+	console.log(pathName)
 
 	return (
-		<footer className="lg:absolute w-full bottom-0 flex items-center justify-center mt-6 px-4 py-[26px]">
+		<footer
+			className={cn(
+				"w-full bottom-0 flex items-center justify-center mt-6 px-4 py-[26px]",
+				{
+					"lg:absolute": pathName === "/projects",
+					absolute: pathName !== "/projects",
+				}
+			)}
+		>
 			<p className="text-xs">{actualYear} @ Gustavo Silvério</p>
 		</footer>
 	)
