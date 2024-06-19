4c661fc306afb2af35f340fafdccbaa52556bb9b
GustavoSilverio <gustavosk290905@gmail.com>
2024-06-19 12:33:37 -0300

    add active col

--- src/api/controllers/project.ts
+++ src/api/controllers/project.ts
@@ -4,7 +4,11 @@ import { prisma } from "../services/prisma"
 
 export const getAllProjects = async () => {
 	try {
-		return await prisma.project.findMany()
+		return await prisma.project.findMany({
+			where: {
+				active: true,
+			},
+		})
 	} catch (err) {
 		console.error(err)
 	}
