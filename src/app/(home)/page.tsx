03ea9aa19bcd57f5fa96300ee357bafecb18d030
GustavoSilverio <gustavosk290905@gmail.com>
2024-08-17 00:51:11 -0300

    corrections

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
 					</h1>
 
 					<p className="leading-5">
-						I'm a full stack dev from São Paulo, Brasil, I work with
-						web development for 2 years.
+						I'm a full stack developer from São Paulo, Brasil and I
+						work with web development for 2 years.
 					</p>
 				</div>
 			</div>
