88d928f44bfe03a9e5f51277538741fe240b52bf
Gustavo Silverio Firmino Sotelo de Oliveira <2403294@SALAS.AULAS>
2024-09-04 09:27:37 -0300

    structure finished

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { MainContainer } from "@/components/mainContainer/mainContainer"
-import { CircleHelp } from "lucide-react"
+import { CircleHelp, X } from "lucide-react"
 import Image from "next/image"
 import { motion, Variants } from "framer-motion"
 import { useState } from "react"
@@ -24,7 +24,29 @@ export default function Home() {
 				className="flex flex-col justify-center items-center w-full h-full"
 				animate={showSummary ? "open" : "closed"}
 			>
-				<motion.div className="animate-fade-in flex flex-col items-center space-y-6 max-w-[454px] w-full">
+				<motion.div
+					className="animate-fade-in flex flex-col items-center space-y-6 max-w-[454px] w-full absolute top-auto"
+					variants={{
+						open: {
+							opacity: 0,
+							scale: 0,
+						},
+
+						closed: {
+							opacity: 1,
+							scale: 1,
+
+							transition: {
+								delay: .2,
+							},
+						},
+					}}
+					transition={{
+						type: "spring",
+						bounce: 0.3,
+						duration: 0.3
+					}}
+				>
 					<Image
 						priority
 						src="/images/profile.png"
@@ -69,69 +91,136 @@ export default function Home() {
 					</div>
 				</motion.div>
 
-				<motion.div className="flex flex-col items-center justify-center w-full max-h-[372px] px-16">
+				<motion.div className="flex flex-col items-center justify-center w-full h-full max-h-[372px] px-16">
 					<motion.div
-						className="flex flex-col gap-3 bg-slate-900 rounded-md w-full h-full p-4"
+						className="bg-slate-900 rounded-md w-full h-full p-4 relative"
 						initial={false}
 						variants={{
 							open: {
 								opacity: 1,
 								scale: 1,
 								y: -38,
-								display: "flex",
 
 								transition: {
 									type: "spring",
 									bounce: 0.3,
-									delayChildren: 0.65,
-									staggerChildren: 0.05,
+									delay: .2,
 								},
 							},
 
 							closed: {
-								display: "none",
 								opacity: 0,
 								scale: 0,
 								y: 0,
+
+								transition: {
+									type: "spring",
+									bounce: 0.3,
+								},
 							},
 						}}
 					>
-						<motion.p
-							variants={itemVariant}
-							transition={{ type: "spring" }}
+						<motion.button
+							className="absolute right-2 top-2"
+							onClick={() => setShowSummary(false)}
+							whileHover={{
+								scale: 1.2
+							}}
+							transition={{
+								type: 'spring',
+								bounce: 0.3,
+							}}
 						>
-							Some text here!
-						</motion.p>
-						<motion.p
-							variants={itemVariant}
-							transition={{ type: "spring" }}
+							<span>
+								<X className="w-[20px]" />
+							</span>
+						</motion.button>
+
+						<motion.div
+							className="flex flex-col gap-3 overflow-y-auto h-full mr-4 pr-2"
+							variants={{
+								open: {
+									transition: {
+										delayChildren: 0.65,
+										staggerChildren: 0.05,
+									}
+								},
+								closed: {},
+							}}
 						>
-							Some text here!
-						</motion.p>
-						<motion.p
-							variants={itemVariant}
-							transition={{ type: "spring" }}
-						>
-							Some text here!
-						</motion.p>
-						<motion.p
-							variants={itemVariant}
-							transition={{ type: "spring" }}
-						>
-							Some text here!
-						</motion.p>
-						<motion.p
-							variants={itemVariant}
-							transition={{ type: "spring" }}
-						>
-							Some text here!
-						</motion.p>
-						<motion.p
-							variants={itemVariant}
-							transition={{ type: "spring" }}
-						>
-							Some text here!
-						</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui sapiente quisquam amet ratione reiciendis hic id, autem incidunt commodi voluptas possimus, nulla accusamus laborum ducimus tempore aliquid obcaecati laudantium doloribus vel dolore porro? At vitae itaque enim reprehenderit praesentium expedita libero impedit. Ab obcaecati id repellat praesentium aspernatur magnam velit expedita! Fuga earum deleniti nisi facere in. Enim dicta numquam cumque tempora asperiores hic dolor error doloremque earum nam commodi iste, perferendis a, illo alias nostrum quae aliquid molestias eaque? Modi, reprehenderit eaque enim accusamus consequatur laborum animi corporis et quam nostrum eum sunt, atque in magnam fugiat ab asperiores.
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+							<motion.p
+								variants={itemVariant}
+								transition={{ type: "spring" }}
+							>
+								Some text here!
+							</motion.p>
+						</motion.div>
 					</motion.div>
 				</motion.div>
 			</motion.div>
