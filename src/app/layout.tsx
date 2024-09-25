448059fb58fd5138dae6beb696add6cc540f68fb
GustavoSilverio <gustavosk290905@gmail.com>
2024-09-24 23:28:25 -0300

    clarity added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { SocialMedias } from "@/components/socialMedias"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 import { GoUp } from "@/components/goUp"
+import Script from "next/script"
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -54,6 +55,19 @@ export default function RootLayout({
 				<meta
 					property="og:url"
 					content="https://www.gsilverio.com/"
+				/>
+				<Script
+					id="clarity-script"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(c,l,a,r,i,t,y){
+							c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+							t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+							y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+							})(window, document, "clarity", "script", "i2bgq9i36n");
+						`,
+					}}
 				/>
 			</head>
 			<body className={`${inter.className}`}>
